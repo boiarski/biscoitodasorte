7e1e8db9557fac06391b8ccfda2378c75be25852
Boiarski <diego_boiarski@hotmail.com>
2024-02-08 19:58:25 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const fortuneCookie = document.querySelector('.fortuneCookie');
+const newFortune = document.querySelector('.newFortune');
+
+newFortune.addEventListener('click', handleResetClick);
+
+function handleResetClick(){
+    toggleScreen();
+}
+
+function toggleScreen() {
+    screen1.classList.toggle("hide");
+    screen2.classList.toggle("hide");
+}
+
